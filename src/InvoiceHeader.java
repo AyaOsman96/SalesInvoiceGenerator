7968945aba7520d0eaa55eab3571db3ab3789b10
AyaOsman <aya.osman96@gmail.com>
2022-11-26 19:32:33 +0200

    First Commit

--- src/InvoiceHeader.java
+++ src/InvoiceHeader.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class InvoiceHeader {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ArrayList;
+
+public class InvoiceHeader {
+
+    private int invNo;
+    private Date date;
+    //private DateFormat d= new SimpleDateFormat("dd-MM-yyyy");
+    private String cusName;
+    //private ArrayList<Invoice_Line>lines ;
+    private int totalNo;
+
+    public InvoiceHeader() {
+    }
+
+    public InvoiceHeader(int num,Date date, String name){
+        super();
+        this.invNo = num;
+        this.date = date;
+        this.cusName = name;
+    }
+
+    public int getInvNo() {
+        return invNo;
+    }
+
+    public void setInvNo(int invNo) {
+        this.invNo = invNo;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCusName() {
+        return cusName;
+    }
+
+    public void setCusName(String cusName) {
+        this.cusName = cusName;
+    }
+
+    public int getTotalNo() {
+        return totalNo;
+    }
+
+    public void setTotalNo(int totalNo) {
+        this.totalNo = totalNo;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceHeader{" +
+                "Invoice Number: " + invNo +
+                ", Date: " + date +
+                ", Customer Name: " + cusName + " }";
+    }
 }
--- src/InvoiceTable.java
+++ src/InvoiceTable.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class InvoiceTable {
+import java.util.Date;
+
+public class InvoiceTable {
+
+//    private int invNum;
+//    private Date date;
+//    private String cusName;
+//    private float InvTotal;
+    private float itmPrice;
+    private String itmName;
+    private int count;
+    private int itmTotal;
+
+    public InvoiceTable(){
+
+    }
+
+    public InvoiceTable(int x){
+
+    }
+
+    public float getItmPrice() {
+        return itmPrice;
+    }
+
+    public void setItmPrice(float itmPrice) {
+        this.itmPrice = itmPrice;
+    }
+
+    public String getItmName() {
+        return itmName;
+    }
+
+    public void setItmName(String itmName) {
+        this.itmName = itmName;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getItmTotal() {
+        return itmTotal;
+    }
+
+    public void setItmTotal(int itmTotal) {
+        this.itmTotal = itmTotal;
+    }
 }
